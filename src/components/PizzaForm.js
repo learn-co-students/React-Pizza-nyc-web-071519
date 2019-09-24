678a10d7220835341ce6020ca4a8818566feade6
Andrew Santos <asantos@alumni.nd.edu>
2019-09-24 09:11:22 -0400

    Done.

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,16 +1,52 @@
 import React from "react"
 
-const PizzaForm = () => {
-  return(
+class PizzaForm extends React.Component{
+
+  state = {
+    id: this.props.pizza.id,
+    topping: this.props.pizza.topping,
+    size: this.props.pizza.size,
+    vegetarian: this.props.pizza.vegetarian
+  }
+
+  componentDidUpdate(prevProps) {
+      if (this.props.pizza.id !== prevProps.pizza.id) {
+        this.setState({
+          id: this.props.pizza.id,
+          topping: this.props.pizza.topping,
+          size: this.props.pizza.size,
+          vegetarian: this.props.pizza.vegetarian
+        })
+    }
+  }
+
+  changeHandler = (e) => {
+    if (e.target.name === 'vegetarian'){
+      let value = (e.target.value === 'Vegetarian' ? true : false)
+      this.setState({
+        vegetarian: value
+      })
+    } else {
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }
+  }
+
+  submitHandler = () => {
+    this.props.updatePizza(this.state)
+  }
+
+  render(){
+    return(
       <div className="form-row">
         <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
+            <input type="text" className="form-control" name='topping' onChange={this.changeHandler} placeholder="Pizza Topping" value={
+                this.state.topping
               }/>
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select value={this.state.size} name='size' onChange={this.changeHandler} className="form-control">
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -18,24 +54,26 @@ const PizzaForm = () => {
         </div>
         <div className="col">
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+            <input className="form-check-input" type="radio" name='vegetarian' onChange={this.changeHandler} value="Vegetarian" checked={this.state.vegetarian}/>
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+            <input className="form-check-input" type="radio" name='vegetarian' onChange={this.changeHandler} value="Not Vegetarian" checked={!this.state.vegetarian}/>
             <label className="form-check-label">
               Not Vegetarian
             </label>
           </div>
         </div>
         <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+          <button type="submit" className="btn btn-success" onClick={this.submitHandler}>Submit</button>
         </div>
       </div>
 
-  )
+    )
+  }
+  
 }
 
 export default PizzaForm
--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -2,7 +2,34 @@ import React, { Component } from 'react';
 import Pizza from '../components/Pizza'
 class PizzaList extends Component {
 
+  state ={
+    pizzaList: []
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3500/pizzas')
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({
+        pizzaList: data
+      })
+    })
+  }
+
+  componentDidUpdate(prevProps){
+    if (this.props !== prevProps)
+    fetch('http://localhost:3500/pizzas')
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({
+        pizzaList: data
+      })
+    })
+  }
+  
+
   render() {
+    const pizzaComponents = this.state.pizzaList.map(pizza => <Pizza key={pizza.id} pizza={pizza} clickHandler={this.props.clickHandler}/>)
     return (
       <table className="table table-striped">
         <thead>
@@ -15,7 +42,7 @@ class PizzaList extends Component {
         </thead>
         <tbody>
           {
-            //render Pizza here
+            pizzaComponents
           }
         </tbody>
       </table>
